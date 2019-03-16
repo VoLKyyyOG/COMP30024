1b487b5eb38e7285000eab4e27be71670f4280bf
Akira Wang <30443254+akiratwang@users.noreply.github.com>
2019-03-16 12:50:01 +1100

    t

---
+++ Chexers/Part A/moves.py
@@ -0,0 +1,61 @@
+# def move():
+#     from location, if move valid:
+#         return piece new coordinate
+# 
+# def jump():
+#     from location, if jump valid:
+#     return piece new coordinate
+# 
+# def exit():
+#     from location, if exit valid:
+#     return REMOVE PIECE
+
+
+# Returns goals given player colour
+def find_goal(player, data):
+    # Initialise goals
+    from collections import defaultdict
+    goals = defaultdict(list)
+    goals["red"].append([[3,r] for r in range(-3, 1)])
+    goals["blue"].append([[-3,0],[-2,1],[-1,2],[0,3]])
+    goals["green"].append([[q,3] for q in range(-3, 1)])
+
+    # Default player goal
+    player_goal = goals[player][0]
+
+    # Check if goal not blocked by piece
+    player_goal = [i for i in player_goal if i not in data["blocks"]]
+
+    return player_goal
+
+# Possible moves from current location
+def possible_moves(data):
+    player_pos = data["pieces"]
+    block_pos = data["blocks"]
+    
+    for i in player_pos:
+        print("Player coordinate: ",i) #i is coordinate to be fed into adj_hex
+
+        
+        adj_hex(i)
+        print("**********************************************************")
+        # add 1 to surrounding space
+        # check if there is block
+        # if no block:
+            # return move
+        # if block:
+            # skippable?
+                # return jump
+
+def adj_hex(coordinate):
+    valid_coordinates = [[q, r] for q in range(-3,4) for r in range(-3,4)]
+    qr1 = [coordinate[0] + 1, coordinate[1]]
+    qr2 = [coordinate[0] + 1, coordinate[1] - 1]
+    qr3 = [coordinate[0], coordinate[1] - 1]
+    qr4 = [coordinate[0] - 1, coordinate[1]]
+    qr5 = [coordinate[0] - 1, coordinate[1] + 1]
+    qr6 = [coordinate[0], coordinate[1] + 1]
+    adj_hexes = [qr1,qr2,qr3,qr4,qr5,qr6]
+    print([i for i in [j for j in adj_hexes] if i in valid_coordinates])
+
+
--- Chexers/Part A/search.py
+++ Chexers/Part A/search.py
@@ -8,121 +8,26 @@ Team: _blank_
 
 import json
 import sys
+#################
+from print_debug import debug, print_board
+from moves import possible_moves, find_goal
 
 def main():
     with open(sys.argv[1]) as file:
         data = json.load(file)
-
+        print(data)
     # TODO: Search for and output winning sequence of moves
     # ...
-
-    # track taken moves with list
-    # UCS search?
-    # starting piece "pieces" -> [[-1,2]] indicates a single piece on the hex indexed (-1, 2)
-    # "blocks" specifying the position of any blocks on the board
-    # -> For example, the entry "blocks": [[0,1], [2,-1]] indicates blocks on the hexes indexed (0, 1) and (2, -1)
-    # Use command: python search.py test-files/test.json         to run it via terminal
+    # Use command: python search.py test-files/test.json to run it via terminal
     print_board(debug(data), message = "Test Board", debug=True)
 
-    # need to create a "move", "jump" and "exit" action 
-    # define functions that read the coordinate of pieces and blocks and check if valid
+    # Find the player goal
+    player = data["colour"]
+    player_goal = find_goal(player, data)
+    print("Player Gaol: ",player_goal)
+    print("**********************************************************")
 
-    ############ search algo below i guess
-
-
-# The print_board dictionary should be of format {(q, r): color_of_piece}
-# The purpose of this is to visualise it
-def debug(data):
-    board_dict = {}
-    player_pieces = [tuple(i) for i in data['pieces']]
-    blocks = [tuple(i) for i in data['blocks']]
-    for i in player_pieces:
-        board_dict[i] = data['colour']
-    for i in blocks:
-        board_dict[i] = 'B'
-    return board_dict
-
-def print_board(board_dict, message="", debug=False, **kwargs):
-    """
-    Helper function to print a drawing of a hexagonal board's contents.
-    
-    Arguments:
-
-    * `board_dict` -- dictionary with tuples for keys and anything printable
-    for values. The tuple keys are interpreted as hexagonal coordinates (using 
-    the axial coordinate system outlined in the project specification) and the 
-    values are formatted as strings and placed in the drawing at the corres- 
-    ponding location (only the first 5 characters of each string are used, to 
-    keep the drawings small). Coordinates with missing values are left blank.
-
-    Keyword arguments:
-
-    * `message` -- an optional message to include on the first line of the 
-    drawing (above the board) -- default `""` (resulting in a blank message).
-    * `debug` -- for a larger board drawing that includes the coordinates 
-    inside each hex, set this to `True` -- default `False`.
-    * Or, any other keyword arguments! They will be forwarded to `print()`.
-    """
-
-    # Set up the board template:
-    if not debug:
-        # Use the normal board template (smaller, not showing coordinates)
-        template = """# {0}
-#           .-'-._.-'-._.-'-._.-'-.
-#          |{16:}|{23:}|{29:}|{34:}| 
-#        .-'-._.-'-._.-'-._.-'-._.-'-.
-#       |{10:}|{17:}|{24:}|{30:}|{35:}| 
-#     .-'-._.-'-._.-'-._.-'-._.-'-._.-'-.
-#    |{05:}|{11:}|{18:}|{25:}|{31:}|{36:}| 
-#  .-'-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'-.
-# |{01:}|{06:}|{12:}|{19:}|{26:}|{32:}|{37:}| 
-# '-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'
-#    |{02:}|{07:}|{13:}|{20:}|{27:}|{33:}| 
-#    '-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'
-#       |{03:}|{08:}|{14:}|{21:}|{28:}| 
-#       '-._.-'-._.-'-._.-'-._.-'-._.-'
-#          |{04:}|{09:}|{15:}|{22:}|
-#          '-._.-'-._.-'-._.-'-._.-'"""
-    else:
-        # Use the debug board template (larger, showing coordinates)
-        template = """# {0}
-#              ,-' `-._,-' `-._,-' `-._,-' `-.
-#             | {16:} | {23:} | {29:} | {34:} | 
-#             |  0,-3 |  1,-3 |  2,-3 |  3,-3 |
-#          ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-.
-#         | {10:} | {17:} | {24:} | {30:} | {35:} |
-#         | -1,-2 |  0,-2 |  1,-2 |  2,-2 |  3,-2 |
-#      ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-. 
-#     | {05:} | {11:} | {18:} | {25:} | {31:} | {36:} |
-#     | -2,-1 | -1,-1 |  0,-1 |  1,-1 |  2,-1 |  3,-1 |
-#  ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-.
-# | {01:} | {06:} | {12:} | {19:} | {26:} | {32:} | {37:} |
-# | -3, 0 | -2, 0 | -1, 0 |  0, 0 |  1, 0 |  2, 0 |  3, 0 |
-#  `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' 
-#     | {02:} | {07:} | {13:} | {20:} | {27:} | {33:} |
-#     | -3, 1 | -2, 1 | -1, 1 |  0, 1 |  1, 1 |  2, 1 |
-#      `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' 
-#         | {03:} | {08:} | {14:} | {21:} | {28:} |
-#         | -3, 2 | -2, 2 | -1, 2 |  0, 2 |  1, 2 | key:
-#          `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' ,-' `-.
-#             | {04:} | {09:} | {15:} | {22:} |   | input |
-#             | -3, 3 | -2, 3 | -1, 3 |  0, 3 |   |  q, r |
-#              `-._,-' `-._,-' `-._,-' `-._,-'     `-._,-'"""
-
-    # prepare the provided board contents as strings, formatted to size.
-    ran = range(-3, +3+1)
-    cells = []
-    for qr in [(q,r) for q in ran for r in ran if -q-r in ran]:
-        if qr in board_dict:
-            cell = str(board_dict[qr]).center(5)
-        else:
-            cell = "     " # 5 spaces will fill a cell
-        cells.append(cell)
-
-    # fill in the template to create the board drawing, then print!
-    board = template.format(message, *cells)
-    print(board, **kwargs)
-
+    possible_moves(data)
 
 # when this module is executed, run the `main` function:
 if __name__ == '__main__':
